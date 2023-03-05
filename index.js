afbc3c03ce8cddf4cbd4dafef87f7951d16f1ff1
metatron3 <122368738+metatron3@users.noreply.github.com>
2023-03-05 08:23:21 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+
+let edad
+
+alert("Atención");
+if (edad > 18)
+console.log
+const sure = confirm ("¿Sos mayor de 18 años?")
+if (sure) {
+    const name = prompt("¿Cuál es tu nombre?");
+    alert("Bienvenido");
+}
+
+
+document.querySelector("#Cerrar").addEventListener("click", function(){
+document.querySelector(".popup").style.display = "none";
+})
